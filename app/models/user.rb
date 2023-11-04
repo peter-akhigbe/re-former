db127e2fcf3c87009c5d570a375f40cf85c984c2
Peter Akhigbe <akhigbe37@gmail.com>
2023-11-04 14:25:12 +0100

    Build user route and controller with new and create action

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  # Add validations for presence to each field in the model
+  validates :name, :email, :password, presence: true
+  # Add validations for uniqueness to email
+  validates :email, uniqueness: true
+  # Add validations for length to password
+  validates :password, length: {minimum: 6}
 end
