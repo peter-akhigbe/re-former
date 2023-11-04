569ed3a9d0852cef27dba5262160dddc3c18542a
Peter Akhigbe <akhigbe37@gmail.com>
2023-11-04 15:58:38 +0100

    HTML form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,3 @@
 class User < ApplicationRecord
-  # Add validations for presence to each field in the model
-  validates :name, :email, :password, presence: true
-  # Add validations for uniqueness to email
-  validates :email, uniqueness: true
-  # Add validations for length to password
-  validates :password, length: {minimum: 6}
+  validates :username, :email, :password, presence: true
 end
