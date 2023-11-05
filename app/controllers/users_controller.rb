c5aee05130663088878e169ebe60b2c3c6b01abe
Peter Akhigbe <akhigbe37@gmail.com>
2023-11-05 11:20:27 +0100

    Editing with form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      # redirect_to @article
+      puts "User updated"
+    else
+      # render :edit, status: :unprocessable_entity
+      puts "User not updated"
+    end
+  end
+
   private
 
   def user_params
